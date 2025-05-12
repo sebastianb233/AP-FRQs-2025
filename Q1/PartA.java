aa9ebe69a4d57d1cc5440b9f6e406c14e72fb045
sebastianb233 <sebastianb233@nycstudents.net>
2025-05-12 10:10:17 -0400

    Update PartA.java

--- Q1/PartA.java
+++ Q1/PartA.java
@@ -1,4 +1,12 @@
-
 public int WalkDogs(int hour){
-        
-}
+        //find out from the DogWalkCompanyHow many dogs at hour
+        int numDogs = company.newAvalableDogs(hour);
+        // if walkercan talk more dogs than available
+        if(maxDogs > numDogs){
+            company.updateDogs(hour,numDogs);
+            return numDogs;
+        }
+        //if there are more (or equal) dogs than the walker can handle all of than.
+        company.updateDogs(hour,maxDogs);
+        return maxDogs;
+    }
