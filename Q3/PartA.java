c3786463df04aae58ac2617219dd57d7f029fcf5
sebastianb233 <sebastianb233@nycstudents.net>
2025-05-13 16:50:31 -0400

    Update PartA.java

--- Q3/PartA.java
+++ Q3/PartA.java
@@ -1,1 +1,6 @@
-
+public Round(String[] names){
+        competitorList= new ArrayList<>();
+        for(int i=0;i<names.length;i++){
+            competitorList.add(new Competiter(names[i],i+1));
+        }
+    }
