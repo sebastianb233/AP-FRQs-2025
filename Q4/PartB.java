c80008b6d13cf0277b47101a92232c713cf13abb
sebastianb233 <sebastianb233@nycstudents.net>
2025-05-13 17:18:48 -0400

    Update PartB.java

--- Q4/PartB.java
+++ Q4/PartB.java
@@ -1,1 +1,14 @@
-
+public boolean clearPair(int row, int col){
+        for(int r=row;r<puzzle.length;r++){
+            for(int c=col;c<puzzle[r].length;c++){
+                if(r !=row || c!=col){} 
+                 if(puzzle[row][col]==puzzle[r][c]||puzzle[row][col]+puzzle[r][c]==10){
+                    puzzle[row][col]=0;
+                    puzzle[r][c]=0;
+                    return true;
+                 }
+                } 
+            }
+        }
+        return false;
+    }
