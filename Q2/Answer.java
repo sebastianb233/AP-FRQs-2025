6e05cdc513c10b1fb8d9844e87daf04d93fa2282
sebastianb233 <sebastianb233@nycstudents.net>
2025-05-13 10:13:38 -0400

    Update Answer.java

--- Q2/Answer.java
+++ Q2/Answer.java
@@ -1,1 +1,30 @@
+public class SignedText{
+    private String fristName;
+    private String lastName;
+    
 
+    public SignedText(String Text1,String Text2){
+        fristName=Text1;
+        lastName=Text2;
+    }
+
+    public String getSignature(){
+        if(fristName.equals("")){
+            return lastName;
+        }else{
+            return fristName.substring(0,1)+" - "+lastName;
+        }
+    }
+
+    public String addSignature(String text){
+        if(getSignature().indexOf(text)==-1){
+            return getSignature()+text;
+        }
+        int index=text.indexOf(getSignature);
+        if(index== (text.length()-getSignature().length())){
+            return text;
+        }else if(index==0){
+            return text.substring(getSignature())+getSignature();
+        }
+    }
+}
