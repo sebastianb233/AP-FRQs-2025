0b3d9d82055bbdc97a939ef081000ffcae3ec0dd
sebastianb233 <sebastianb233@nycstudents.net>
2025-05-13 17:04:38 -0400

    Update PartB.java

--- Q3/PartB.java
+++ Q3/PartB.java
@@ -1,1 +1,13 @@
-
+public ArrayList<Match> buildMatches(){
+        ArrayList<Match>end = new ArrayList<Match>();
+        int i=0;
+        if(competitorList.size() % 2 !=0){
+            i=1;
+        }
+        for(int y=0;y<competitorList.size()/2;y++){
+            Competiter first=competitorList.get(i+1);
+            Competiter second= competitorList.get((competitorList.size()-1)-i);
+            end.add(new Match(first,second));
+        }
+        return end;
+    }
