1874f222ab7517c56293a599dcdfc9f5662104c7
sebastianb233 <sebastianb233@nycstudents.net>
2025-05-13 17:11:17 -0400

    Update PartA.java

--- Q4/PartA.java
+++ Q4/PartA.java
@@ -1,1 +1,8 @@
-
+public SumOrSameGame(int numRows, int numCols){
+        puzzle = new int[numRows][numCols];
+        for(int row=0;row<puzzle.length;row++){
+            for(int col=0;col<puzzle[0].length;col++){
+                puzzle[row][col]=(int)(Math.ramdom()*9)+1;
+            }
+        }
+    }
