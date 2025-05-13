d2b1870acbab00e954bfaf60062e4c2358182081
sebastianb233 <sebastianb233@nycstudents.net>
2025-05-13 09:36:12 -0400

    Update PartB.java

--- Q1/PartB.java
+++ Q1/PartB.java
@@ -1,1 +1,10 @@
-
+public int dogWalkShift(int startHour, int endHour){
+        int moneyEarned=0;
+        for(int hour=startHour;hour <= endHour;hour++){
+            int dogs =walkDogs(hour); 
+            if(dogs == maxDogs || (hour >=9 && <=17)){
+                moneyeEarned +=3;
+            }
+        }
+        return moneyEarned;
+    }
